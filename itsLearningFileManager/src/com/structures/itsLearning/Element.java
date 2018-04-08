9cc11ea94b22360c99e98cdce4f5cba960365374
Kisagi <kaan.biter@stu.bahcesehir.edu.tr>
2018-04-08 14:39:31 +0300

    Implemented data structure for resources

---
+++ itsLearningFileManager/src/com/structures/itsLearning/Element.java
@@ -0,0 +1,56 @@
+package com.structures.itsLearning;
+
+public class Element {
+	
+	// Attributes
+	private String name;
+	private String path;
+	private String type;
+	private String href;
+	
+	// Default Constructor
+	public Element(){
+		this.setName(null);
+		this.setPath(null);
+		this.setType(null);
+		this.setHref(null);
+	}
+	
+	// Parametric Constructor
+	public Element(String name, String path, String type, String href){
+		this.setName(name);
+		this.setPath(path);
+		this.setType(type);
+		this.setHref(href);
+	}
+
+	// Get & Set
+	// Name
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	// Path
+	public String getPath() {
+		return path;
+	}
+	public void setPath(String path) {
+		this.path = path;
+	}
+	// Type
+	public String getType() {
+		return type;
+	}
+	public void setType(String type) {
+		this.type = type;
+	}
+	// Href
+	public String getHref() {
+		return href;
+	}
+	public void setHref(String href) {
+		this.href = href;
+	}
+}
