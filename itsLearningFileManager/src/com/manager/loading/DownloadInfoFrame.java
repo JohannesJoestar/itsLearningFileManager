1a1b171d4386f52e85ef0fc4b42c42aa7518a05c
Kisagi <kaan.biter@stu.bahcesehir.edu.tr>
2018-04-11 21:46:24 +0300

    Added JProgressBar

--- itsLearningFileManager/src/com/manager/loading/DownloadInfoFrame.java
+++ itsLearningFileManager/src/com/manager/loading/DownloadInfoFrame.java
@@ -1,13 +1,11 @@
 package com.manager.loading;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.JProgressBar;
 
 public class DownloadInfoFrame extends JFrame {
 
@@ -15,13 +13,34 @@ public class DownloadInfoFrame extends JFrame {
 	private JPanel contentPane;
 	private JLabel lblInformation;
 	private JLabel lblFilename;
-
+	private JProgressBar progressBar;
 	/**
 	 * Create the frame.
 	 */
-	public DownloadInfoFrame() {
+	public DownloadInfoFrame(int min, int max) {
+		
+		this.initialiseComponents();
+		progressBar.setMinimum(min);
+		progressBar.setMaximum(max);
+		
+	}
+	
+	// Update the label
+	public void update(String file){
+		lblFilename.setText(file);
+	}
+	
+	// Update the value
+	public void setValue(int value){
+		progressBar.setValue(value);
+	}
+	
+	// Components
+	public void initialiseComponents(){
+		
+		setTitle("Downloading...");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 826, 141);
+		setBounds(100, 100, 826, 200);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
@@ -29,16 +48,25 @@ public class DownloadInfoFrame extends JFrame {
 		
 		lblInformation = new JLabel("Downloading and moving file:");
 		lblInformation.setFont(new Font("Tahoma", Font.PLAIN, 29));
-		lblInformation.setBounds(10, 11, 392, 82);
+		lblInformation.setBounds(20, 11, 382, 82);
 		contentPane.add(lblInformation);
 		
 		lblFilename = new JLabel("none");
 		lblFilename.setFont(new Font("Tahoma", Font.BOLD, 20));
 		lblFilename.setBounds(414, 32, 375, 48);
 		contentPane.add(lblFilename);
+		
+		progressBar = new JProgressBar();
+		progressBar.setBounds(20, 104, 769, 46);
+		contentPane.add(progressBar);
+		
+		revalidate();
 	}
 	
-	public void update(String file){
-		lblFilename.setText(file);
+	// Get & Set
+	// Get
+	public JProgressBar getProgressBar(){
+		return this.progressBar;
 	}
+	
 }
