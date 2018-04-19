a98bb6338e00d19abdca0e5f1ed7bd70d7f0b7e4
Kisagi <kaan.biter@stu.bahcesehir.edu.tr>
2018-04-19 20:25:54 +0300

    Touch-ups on the DownloadDialog

--- itsLearningFileManager/src/com/manager/frames/DownloadDialog.java
+++ itsLearningFileManager/src/com/manager/frames/DownloadDialog.java
@@ -13,12 +13,15 @@ import javax.swing.JList;
 import javax.swing.JTextField;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.SoftBevelBorder;
 
 public class DownloadDialog extends JFrame {
 
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
+	private JTextField txtElementNameDownload;
+	private JTextField txtElementTypeDownload;
 
 	/**
 	 * Launch the application.
@@ -40,70 +43,82 @@ public class DownloadDialog extends JFrame {
 	 * Create the frame.
 	 */
 	public DownloadDialog() {
+		setTitle("Downloader");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 405, 410);
+		setBounds(100, 100, 450, 512);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(10, 11, 369, 338);
-		contentPane.add(panel);
-		panel.setLayout(null);
+		JPanel pnlChanges = new JPanel();
+		pnlChanges.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		pnlChanges.setBounds(10, 11, 410, 441);
+		contentPane.add(pnlChanges);
+		pnlChanges.setLayout(null);
 		
 		JLabel lblReviewChanges = new JLabel("Review Changes");
 		lblReviewChanges.setBounds(10, 11, 149, 22);
-		panel.add(lblReviewChanges);
+		pnlChanges.add(lblReviewChanges);
 		lblReviewChanges.setFont(new Font("Tahoma", Font.BOLD, 18));
 		
 		JButton btnUpOneLevel = new JButton("Up One Level");
-		btnUpOneLevel.setBounds(10, 51, 153, 23);
-		panel.add(btnUpOneLevel);
+		btnUpOneLevel.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnUpOneLevel.setBounds(10, 36, 196, 23);
+		pnlChanges.add(btnUpOneLevel);
 		
-		JList list = new JList();
-		list.setBounds(10, 92, 159, 235);
-		panel.add(list);
+		JList listChanges = new JList();
+		listChanges.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		listChanges.setBounds(10, 64, 196, 367);
+		pnlChanges.add(listChanges);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(179, 120, 180, 101);
-		panel.add(panel_1);
-		panel_1.setLayout(null);
+		JPanel pnlElementDownload = new JPanel();
+		pnlElementDownload.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		pnlElementDownload.setBounds(218, 125, 180, 101);
+		pnlChanges.add(pnlElementDownload);
+		pnlElementDownload.setLayout(null);
 		
-		JLabel lblName = new JLabel("Name");
+		JLabel lblName = new JLabel("Name:");
+		lblName.setEnabled(false);
 		lblName.setBounds(10, 11, 46, 14);
-		panel_1.add(lblName);
+		pnlElementDownload.add(lblName);
 		
-		JLabel lblType = new JLabel("Type");
-		lblType.setBounds(10, 39, 46, 14);
-		panel_1.add(lblType);
+		JLabel lblType = new JLabel("Type:");
+		lblType.setEnabled(false);
+		lblType.setBounds(10, 37, 46, 14);
+		pnlElementDownload.add(lblType);
 		
-		textField = new JTextField();
-		textField.setBounds(64, 8, 106, 20);
-		panel_1.add(textField);
-		textField.setColumns(10);
+		txtElementNameDownload = new JTextField();
+		txtElementNameDownload.setEnabled(false);
+		txtElementNameDownload.setBounds(64, 8, 106, 20);
+		pnlElementDownload.add(txtElementNameDownload);
+		txtElementNameDownload.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(64, 36, 106, 20);
-		panel_1.add(textField_1);
-		textField_1.setColumns(10);
+		txtElementTypeDownload = new JTextField();
+		txtElementTypeDownload.setEnabled(false);
+		txtElementTypeDownload.setBounds(64, 34, 106, 20);
+		pnlElementDownload.add(txtElementTypeDownload);
+		txtElementTypeDownload.setColumns(10);
 		
 		JButton btnRemove = new JButton("Remove");
+		btnRemove.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnRemove.setEnabled(false);
 		btnRemove.setBounds(46, 67, 89, 23);
-		panel_1.add(btnRemove);
+		pnlElementDownload.add(btnRemove);
 		
-		JButton btnDownloadNewFiles = new JButton("Download New Files");
-		btnDownloadNewFiles.addActionListener(new ActionListener() {
+		JButton btnDownload = new JButton("Download New Files");
+		btnDownload.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnDownload.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
 			}
 		});
-		btnDownloadNewFiles.setBounds(199, 232, 149, 23);
-		panel.add(btnDownloadNewFiles);
+		btnDownload.setBounds(234, 239, 149, 23);
+		pnlChanges.add(btnDownload);
 		
 		JLabel lblSelectedElement = new JLabel("Selected Element");
 		lblSelectedElement.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblSelectedElement.setBounds(206, 87, 126, 22);
-		panel.add(lblSelectedElement);
+		lblSelectedElement.setBounds(245, 92, 126, 22);
+		pnlChanges.add(lblSelectedElement);
 	}
 
 }
