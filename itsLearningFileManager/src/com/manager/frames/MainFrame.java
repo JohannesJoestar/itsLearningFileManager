02fc579795e74414a0c9685fe402da5c0ec4f439
Kisagi <kaan.biter@stu.bahcesehir.edu.tr>
2018-04-19 20:16:08 +0300

    Touch-ups on the MainFrame

--- itsLearningFileManager/src/com/manager/frames/MainFrame.java
+++ itsLearningFileManager/src/com/manager/frames/MainFrame.java
@@ -1,8 +1,5 @@
 package com.manager.frames;
 
-import java.awt.BorderLayout;
-import java.awt.EventQueue;
-
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
@@ -12,15 +9,14 @@ import org.openqa.selenium.WebDriver;
 import com.manager.loading.Downloader;
 import com.manager.loading.Loader;
 import com.manager.loading.Settings;
-import javax.swing.JToolBar;
 import javax.swing.JButton;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JTextPane;
 import javax.swing.JList;
 import javax.swing.JTextField;
 import javax.swing.JLabel;
 import java.awt.Font;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.BevelBorder;
+import javax.swing.border.SoftBevelBorder;
 
 public class MainFrame extends JFrame {
 
@@ -31,13 +27,14 @@ public class MainFrame extends JFrame {
 	private Downloader downloader;
 	private Loader loader;
 	private JPanel contentPane;
-	private JTextField textField;
-	private JTextField textField_1;
-	private JTextField textField_2;
-	private JTextField textField_3;
+	private JTextField txtElementNameItsLearning;
+	private JTextField txtElementTypeItsLearning;
+	private JTextField txtElementNameSettings;
+	private JTextField txtElementTypeSettings;
 
 	// Default constructor
 	public MainFrame() {
+		setTitle("itsLearning File Manager");
 		initialiseComponents();
 	}
 	
@@ -53,130 +50,162 @@ public class MainFrame extends JFrame {
 
 	private void initialiseComponents() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 954, 512);
+		setBounds(100, 100, 955, 514);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
 		contentPane.setLayout(null);
 		
-		JPanel panel_YourCpu = new JPanel();
-		panel_YourCpu.setBounds(481, 11, 447, 451);
-		contentPane.add(panel_YourCpu);
-		panel_YourCpu.setLayout(null);
+		JPanel pnlSetting = new JPanel();
+		pnlSetting.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		pnlSetting.setBounds(473, 11, 452, 452);
+		contentPane.add(pnlSetting);
+		pnlSetting.setLayout(null);
 		
-		JList list_1 = new JList();
-		list_1.setBounds(10, 95, 226, 345);
-		panel_YourCpu.add(list_1);
+		JList listSettings = new JList();
+		listSettings.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		listSettings.setBounds(10, 75, 226, 364);
+		pnlSetting.add(listSettings);
 		
-		JLabel lblYourComputer = new JLabel("Your Computer");
-		lblYourComputer.setFont(new Font("Tahoma", Font.BOLD, 20));
-		lblYourComputer.setBounds(10, 11, 183, 31);
-		panel_YourCpu.add(lblYourComputer);
-		
-		JButton btnUpOneLevel_1 = new JButton("Up One Level");
-		btnUpOneLevel_1.setBounds(10, 61, 103, 23);
-		panel_YourCpu.add(btnUpOneLevel_1);
+		JLabel lblSettings = new JLabel("Your Computer");
+		lblSettings.setFont(new Font("Tahoma", Font.BOLD, 20));
+		lblSettings.setBounds(10, 13, 163, 23);
+		pnlSetting.add(lblSettings);
 		
 		JButton btnReload = new JButton("Reload");
-		btnReload.setBounds(133, 61, 103, 23);
-		panel_YourCpu.add(btnReload);
+		btnReload.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnReload.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnReload.setBounds(127, 42, 109, 23);
+		pnlSetting.add(btnReload);
 		
-		JPanel panel_1 = new JPanel();
-		panel_1.setBounds(246, 126, 196, 134);
-		panel_YourCpu.add(panel_1);
-		panel_1.setLayout(null);
+		JPanel pnlElementSettings = new JPanel();
+		pnlElementSettings.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		pnlElementSettings.setBounds(246, 126, 198, 134);
+		pnlSetting.add(pnlElementSettings);
+		pnlElementSettings.setLayout(null);
 		
-		textField_2 = new JTextField();
-		textField_2.setBounds(66, 11, 120, 20);
-		panel_1.add(textField_2);
-		textField_2.setColumns(10);
+		txtElementNameSettings = new JTextField();
+		txtElementNameSettings.setEnabled(false);
+		txtElementNameSettings.setBounds(66, 14, 120, 20);
+		pnlElementSettings.add(txtElementNameSettings);
+		txtElementNameSettings.setColumns(10);
 		
-		textField_3 = new JTextField();
-		textField_3.setBounds(66, 42, 120, 20);
-		panel_1.add(textField_3);
-		textField_3.setColumns(10);
+		txtElementTypeSettings = new JTextField();
+		txtElementTypeSettings.setEnabled(false);
+		txtElementTypeSettings.setBounds(66, 40, 120, 20);
+		pnlElementSettings.add(txtElementTypeSettings);
+		txtElementTypeSettings.setColumns(10);
 		
-		JLabel lblName_1 = new JLabel("Name");
-		lblName_1.setBounds(10, 14, 46, 14);
-		panel_1.add(lblName_1);
+		JLabel lblElementNameSettings = new JLabel("Name:");
+		lblElementNameSettings.setEnabled(false);
+		lblElementNameSettings.setBounds(8, 17, 46, 14);
+		pnlElementSettings.add(lblElementNameSettings);
 		
-		JLabel lblType_1 = new JLabel("Type");
-		lblType_1.setBounds(10, 45, 46, 14);
-		panel_1.add(lblType_1);
+		JLabel lblElementTypeSettings = new JLabel("Type:");
+		lblElementTypeSettings.setEnabled(false);
+		lblElementTypeSettings.setBounds(10, 43, 46, 14);
+		pnlElementSettings.add(lblElementTypeSettings);
 		
 		JButton btnRemove = new JButton("Remove");
-		btnRemove.setBounds(55, 73, 89, 23);
-		panel_1.add(btnRemove);
+		btnRemove.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnRemove.setEnabled(false);
+		btnRemove.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnRemove.setBounds(59, 73, 89, 23);
+		pnlElementSettings.add(btnRemove);
 		
 		JButton btnRename = new JButton("Rename");
-		btnRename.setBounds(55, 107, 89, 23);
-		panel_1.add(btnRename);
+		btnRename.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnRename.setEnabled(false);
+		btnRename.setFont(new Font("Tahoma", Font.BOLD, 12));
+		btnRename.setBounds(59, 98, 89, 23);
+		pnlElementSettings.add(btnRename);
 		
-		JLabel lblSelectedElement_1 = new JLabel("Selected Element");
-		lblSelectedElement_1.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblSelectedElement_1.setBounds(284, 105, 131, 17);
-		panel_YourCpu.add(lblSelectedElement_1);
+		JLabel lblSelectedSettings = new JLabel("Selected Element");
+		lblSelectedSettings.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblSelectedSettings.setBounds(284, 105, 131, 17);
+		pnlSetting.add(lblSelectedSettings);
 		
-		JButton btnImportChangesFrom = new JButton("Import Changes From itsLearning");
-		btnImportChangesFrom.setBounds(246, 302, 196, 31);
-		panel_YourCpu.add(btnImportChangesFrom);
+		JButton btnImportChanges = new JButton("Import Changes From itsLearning");
+		btnImportChanges.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnImportChanges.setFont(new Font("Tahoma", Font.PLAIN, 11));
+		btnImportChanges.setBounds(248, 273, 192, 31);
+		pnlSetting.add(btnImportChanges);
 		
-		JButton btnChangeInstallationFolder = new JButton("Change Installation Folder");
-		btnChangeInstallationFolder.setBounds(246, 353, 196, 31);
-		panel_YourCpu.add(btnChangeInstallationFolder);
+		JButton btnChangeFolder = new JButton("Change Installation Folder");
+		btnChangeFolder.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnChangeFolder.setBounds(248, 308, 192, 31);
+		pnlSetting.add(btnChangeFolder);
 		
-		JPanel panel_itsLearning = new JPanel();
-		panel_itsLearning.setBounds(10, 11, 452, 451);
-		contentPane.add(panel_itsLearning);
-		panel_itsLearning.setLayout(null);
+		JButton btnUpOneLevel_1 = new JButton("Up One Level");
+		btnUpOneLevel_1.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnUpOneLevel_1.setBounds(10, 42, 107, 23);
+		pnlSetting.add(btnUpOneLevel_1);
 		
-		JList list = new JList();
-		list.setBounds(10, 95, 226, 345);
-		panel_itsLearning.add(list);
+		JPanel pnlItsLearning = new JPanel();
+		pnlItsLearning.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		pnlItsLearning.setBounds(10, 11, 452, 452);
+		contentPane.add(pnlItsLearning);
+		pnlItsLearning.setLayout(null);
 		
-		JButton btnUpOneLevel = new JButton("Up One Level");
-		btnUpOneLevel.setBounds(10, 61, 107, 23);
-		panel_itsLearning.add(btnUpOneLevel);
+		JList listItsLearning = new JList();
+		listItsLearning.setBorder(new BevelBorder(BevelBorder.LOWERED, null, null, null, null));
+		listItsLearning.setBounds(10, 75, 226, 364);
+		pnlItsLearning.add(listItsLearning);
+		
+		JButton btnUpitsLearning = new JButton("Up One Level");
+		btnUpitsLearning.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnUpitsLearning.setBounds(10, 42, 107, 23);
+		pnlItsLearning.add(btnUpitsLearning);
 		
 		JButton btnUpdate = new JButton("Update");
-		btnUpdate.setBounds(127, 61, 109, 23);
-		panel_itsLearning.add(btnUpdate);
+		btnUpdate.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnUpdate.setFont(new Font("Tahoma", Font.BOLD, 13));
+		btnUpdate.setBounds(127, 42, 109, 23);
+		pnlItsLearning.add(btnUpdate);
 		
-		JPanel panel = new JPanel();
-		panel.setBounds(246, 195, 196, 119);
-		panel_itsLearning.add(panel);
-		panel.setLayout(null);
+		JPanel pnlElementItsLearning = new JPanel();
+		pnlElementItsLearning.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		pnlElementItsLearning.setBounds(247, 187, 196, 100);
+		pnlItsLearning.add(pnlElementItsLearning);
+		pnlElementItsLearning.setLayout(null);
 		
 		JButton btnBlockElement = new JButton("Block Element");
-		btnBlockElement.setBounds(38, 87, 113, 23);
-		panel.add(btnBlockElement);
+		btnBlockElement.setBorder(new SoftBevelBorder(BevelBorder.RAISED, null, null, null, null));
+		btnBlockElement.setFont(new Font("Tahoma", Font.BOLD, 11));
+		btnBlockElement.setEnabled(false);
+		btnBlockElement.setBounds(42, 64, 113, 23);
+		pnlElementItsLearning.add(btnBlockElement);
 		
-		textField = new JTextField();
-		textField.setBounds(52, 25, 134, 20);
-		panel.add(textField);
-		textField.setColumns(10);
+		txtElementNameItsLearning = new JTextField();
+		txtElementNameItsLearning.setEnabled(false);
+		txtElementNameItsLearning.setBounds(52, 10, 134, 20);
+		pnlElementItsLearning.add(txtElementNameItsLearning);
+		txtElementNameItsLearning.setColumns(10);
 		
-		textField_1 = new JTextField();
-		textField_1.setBounds(52, 56, 134, 20);
-		panel.add(textField_1);
-		textField_1.setColumns(10);
+		txtElementTypeItsLearning = new JTextField();
+		txtElementTypeItsLearning.setEnabled(false);
+		txtElementTypeItsLearning.setBounds(52, 36, 134, 20);
+		pnlElementItsLearning.add(txtElementTypeItsLearning);
+		txtElementTypeItsLearning.setColumns(10);
 		
-		JLabel lblName = new JLabel("Name");
-		lblName.setBounds(10, 28, 46, 14);
-		panel.add(lblName);
+		JLabel lblElementNameItsLearning = new JLabel("Name:");
+		lblElementNameItsLearning.setEnabled(false);
+		lblElementNameItsLearning.setBounds(10, 13, 46, 14);
+		pnlElementItsLearning.add(lblElementNameItsLearning);
 		
-		JLabel lblType = new JLabel("Type");
-		lblType.setBounds(10, 59, 46, 14);
-		panel.add(lblType);
+		JLabel lblElementTypeItsLearning = new JLabel("Type:");
+		lblElementTypeItsLearning.setEnabled(false);
+		lblElementTypeItsLearning.setBounds(10, 39, 46, 14);
+		pnlElementItsLearning.add(lblElementTypeItsLearning);
 		
 		JLabel lblItslearning = new JLabel("itsLearning");
 		lblItslearning.setFont(new Font("Tahoma", Font.BOLD, 20));
-		lblItslearning.setBounds(10, 11, 139, 39);
-		panel_itsLearning.add(lblItslearning);
+		lblItslearning.setBounds(10, 13, 139, 23);
+		pnlItsLearning.add(lblItslearning);
 		
-		JLabel lblSelectedElement = new JLabel("Selected Element");
-		lblSelectedElement.setFont(new Font("Tahoma", Font.BOLD, 14));
-		lblSelectedElement.setBounds(279, 175, 133, 20);
-		panel_itsLearning.add(lblSelectedElement);
+		JLabel lblSelectedItsLearning = new JLabel("Selected Element");
+		lblSelectedItsLearning.setFont(new Font("Tahoma", Font.BOLD, 14));
+		lblSelectedItsLearning.setBounds(279, 165, 133, 20);
+		pnlItsLearning.add(lblSelectedItsLearning);
 	}
 }
